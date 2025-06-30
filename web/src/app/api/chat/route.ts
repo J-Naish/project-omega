51de089b44a8a589a842701f851ac1e19af16c13
J-Naish <mjyrgd2332@i.softbank.jp>
2025-06-30 16:10:16 +0900

    add mcp client tool

--- web/src/app/api/chat/route.ts
+++ web/src/app/api/chat/route.ts
@@ -1,12 +1,29 @@
 import { anthropic } from "@ai-sdk/anthropic";
 import { streamText } from "ai";
+import { experimental_createMCPClient as createMCPClient } from "ai";
+import { systemPrompt } from "../systemPrompt";
 
 export async function POST(req: Request) {
+
+  const mcpClient = await createMCPClient({
+    transport: {
+      type: "sse",
+      url: ""
+    }
+  });
+
   const { messages } = await req.json();
+
+  const tools = await mcpClient.tools();
 
   const result = streamText({
     model: anthropic("claude-3-7-sonnet-20250219"),
     messages,
+    system: systemPrompt,
+    tools,
+    onFinish: ()=> {
+      mcpClient.close();
+    },
   });
 
   return result.toDataStreamResponse();
---
+++ web/src/app/api/systemPrompt.ts
@@ -0,0 +1,6 @@
+export const systemPrompt = 
+`You are a helpful productivity assistant. Use the following tools to help users manage their tasks if necessary:
+1. Notion
+2. Slack
+3. Google Drive
+`;
