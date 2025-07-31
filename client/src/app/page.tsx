ca9e258ff39da0b842fc844f7f489644eef374bc
J-Naish <mjyrgd2332@i.softbank.jp>
2025-08-01 00:05:20 +0900

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,12 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 export default function Home() {
   return (
     <SidebarProvider>
-      <main className="w-full"></main>
+      <main className="w-full flex justify-center px-6">
+        <div className="w-full max-w-6xl flex flex-col bg-yellow-50">
+          <div className="flex-1">{/* Message Area */}</div>
+          <div className="sticky bottom-0 pb-6">{/* Chat Input */}</div>
+        </div>
+      </main>
     </SidebarProvider>
   );
 }
