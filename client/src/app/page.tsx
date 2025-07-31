fd2317d78cfe5e09f6b65910a568667dcb45caa3
J-Naish <mjyrgd2332@i.softbank.jp>
2025-07-31 23:34:52 +0900

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,5 @@
 import ChatInterface from "@/components/chat/chat-interface";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen">
-      <ChatInterface />
-    </div>
-  );
+  return <ChatInterface />;
 }
