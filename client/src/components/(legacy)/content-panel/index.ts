a308f782893b9cf0da7811597c8b49e75f923fe6
J-Naish <mjyrgd2332@i.softbank.jp>
2025-08-02 18:09:53 +0900

    remove legacy code

--- client/src/components/(legacy)/content-panel/index.ts
+++
@@ -1,4 +0,0 @@
-export { ContentPanel } from "./content-panel";
-export { ContentPanelProvider, useContentPanel } from "./content-panel-context";
-export type { ContentPanelItem } from "./content-panel-context";
-export { PanelHeader } from "./panel-header";
