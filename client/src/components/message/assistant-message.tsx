11aa4c03c526ca95ee05b8f2df95eeaeebd0795f
J-Naish <mjyrgd2332@i.softbank.jp>
2025-08-02 10:54:23 +0900

    Create assistant-message.tsx

---
+++ client/src/components/message/assistant-message.tsx
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import { type Message } from "ai";
+import Markdown from "../markdown";
+import ToolInvocation from "./tool-invocation";
+
+export default function AssistantMessage({ message }: { message: Message }) {
+  return (
+    <div className="w-full">
+      {message.parts?.map((part, index) => (
+        <Fragment key={index}>
+          {part.type === "text" && <Markdown>{part.text}</Markdown>}
+          {part.type === "tool-invocation" && <ToolInvocation part={part} />}
+        </Fragment>
+      ))}
+    </div>
+  );
+}
