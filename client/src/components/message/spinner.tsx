cd289707a0baf1f1eabd58815c6c090fa4973593
J-Naish <mjyrgd2332@i.softbank.jp>
2025-08-02 10:56:54 +0900

    Create spinner.tsx

---
+++ client/src/components/message/spinner.tsx
@@ -0,0 +1,8 @@
+export default function Spinner() {
+  return (
+    <div className="relative">
+      <div className="w-8 h-8 rounded-full bg-gradient-to-r from-purple-500 to-cyan-400 animate-spin blur-[1px] shadow-[0_-3px_12px_0_rgb(186,66,255),0_3px_12px_0_rgb(0,225,255)]"></div>
+      <div className="absolute inset-0 w-8 h-8 rounded-full bg-gray-900 blur-[6px]"></div>
+    </div>
+  );
+}
