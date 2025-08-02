faff1277b93cfc22211967958e17210031d8462f
J-Naish <mjyrgd2332@i.softbank.jp>
2025-08-02 10:50:21 +0900

    Update content-panel.tsx

--- client/src/components/content-panel/content-panel.tsx
+++ client/src/components/content-panel/content-panel.tsx
@@ -1,8 +1,22 @@
-import { Sidebar, SidebarRail } from "@/components/ui/sidebar";
+"use client";
+
+import { X } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Sidebar, SidebarRail, SidebarHeader, useSidebar } from "@/components/ui/sidebar";
 
 export default function ContentPanel() {
+  const { setOpen } = useSidebar();
+
   return (
     <Sidebar side="right">
+      <SidebarHeader>
+        <div className="flex justify-between">
+          <div />
+          <Button variant="ghost" className="cursor-pointer" onClick={() => setOpen(false)}>
+            <X />
+          </Button>
+        </div>
+      </SidebarHeader>
       <SidebarRail />
     </Sidebar>
   );
